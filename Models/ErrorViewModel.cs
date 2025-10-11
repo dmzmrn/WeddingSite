29aeff32bd7864230310b3887da1bbbbb6c2d2b5
Dominic Moreno <dominicmoreno921@gmail.com>
2025-10-11 21:52:48 +0800

    Initialize Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeddingSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
